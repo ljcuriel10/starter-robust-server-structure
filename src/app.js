f9f53ef4308c64592c463373b920b46a5a509c59
Dale 'Ducky' Lotts <dlotts@knightrider.com>
2020-09-25 16:59:37 -0400

    feat: reorganize flips routes

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const app = express();
 const flips = require("./data/flips-data");
 const counts = require("./data/counts-data");
 
+const flipsRouter = require("./flips/flips.router");
+
 app.use(express.json());
 
 app.get("/counts/:countId", (request, response, next) => {
@@ -24,46 +26,7 @@ app.get("/counts", (request, response) => {
   response.json({ data: counts });
 });
 
-app.get("/flips/:flipId", (request, response, next) => {
-  const { flipId } = request.params;
-  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
-
-  if (foundFlip) {
-    response.json({ data: foundFlip });
-  } else {
-    next({
-      status: 404,
-      message: `Flip id not found: ${flipId}`,
-    });
-  }
-});
-
-app.get("/flips", (request, response) => {
-  response.json({ data: flips });
-});
-
-
-function bodyHasResultProperty(request, response, next) {
-  const { data: { result } = {} } = request.body;
-  if (result) {
-    return next();
-  }
-  next({
-    status: 400,
-    message: "A 'result' property is required.",
-  });
-}
-
-app.post("/flips", bodyHasResultProperty, (request, response, next) => {
-  const { data: { result } = {} } = request.body;
-  const newFlip = {
-    id: flips.length + 1, // Assign the next ID
-    result,
-  };
-  flips.push(newFlip);
-  counts[result] = counts[result] + 1; // Increment the counts
-  response.status(201).json({ data: newFlip });
-});
+app.use("/flips", flipsRouter);
 
 // Not found handler
 app.use((request, response, next) => {
