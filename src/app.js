a2b08397a3a15d3a349602dc7e2bdef111aaf452
Luis Javier Curiel <73558171+ljcuriel10@users.noreply.github.com>
2021-02-18 11:40:59 -0800

    static data done

--- src/app.js
+++ src/app.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const app = express();
+const flips = require("./data/flips-data");
+const counts = require("./data/counts-data");
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
+app.use("/flips/:flipId", (req, res, next) => {
+  const { flipId } = req.params;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+  
+  if (!foundFlip) {
+    next(`Flip id not found: ${flidId}`);
+  } else {
+    res.json({ data: foundFlip });
+  }
+});
 
+app.use("/counts/:countId", (req, res, next) => {
+  const { countId } = req.params;
+  const foundCount = counts[countId];
+
+  if(foundCount === undefined) {
+    next(`Count id not found: ${countId}`);
+  } else {
+    res.json({ data: foundCount });
+  }
+})
+
+app.use("/counts", (req, res) => {
+  res.json({ data: counts });
+})
+
+app.use("/flips", (req, res) => {
+  res.json({ data: flips });
+})
 // Not found handler
 app.use((request, response, next) => {
   next(`Not found: ${request.originalUrl}`);
